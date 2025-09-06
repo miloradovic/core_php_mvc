d0dc64e44c09a87a72556fa04b398f57018a5271
miloradovic <miloradovic@outlook.com>
2025-09-06 21:08:44 +0200

    Run PHP CS Fixer

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ namespace App\Routes;
 
 use App\AppRouter;
 
-class Api {
-    public static function register($router) {
+class Api
+{
+    public static function register($router)
+    {
         AppRouter::get('/users', 'UserController@index');
         AppRouter::get('/users/{id}', 'UserController@show');
         AppRouter::post('/users', 'UserController@store');
